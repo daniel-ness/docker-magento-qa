1a7228a0b5622ff4ab539719eb515abcbda02b44
daniel <daniel.r.ness@gmail.com>
2018-08-02 17:01:31 +0100

    (phpcs) 0 return code w/phpcs warnings by default

--- phpcs/entrypoint.sh
+++ phpcs/entrypoint.sh
@@ -4,5 +4,6 @@
 /vendor/bin/phpcs --config-set installed_paths "/vendor/magento-ecg/coding-standard"
 /vendor/bin/phpcs --config-set php7.0_path "$(which php)"
 /vendor/bin/phpcs --config-set show_progress 1
-
+/vendor/bin/phpcs --config-set ignore_errors_on_exit 1
+/vendor/bin/phpcs --config-set ignore_warnings_on_exit 1
 exec $@
